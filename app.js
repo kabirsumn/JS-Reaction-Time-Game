30032bfa30db1b090bea4996c61c82d2f051fea7
kabirsumn <kabirinnow26@gmail.com>
2022-01-18 19:00:59 +0600

    random colored boxes are renering now

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const box = document.getElementById('box');
+const printReactionTime = document.getElementById('printRreactionTime');
+const btn = document.getElementById('btn');
+
 function getRandomColor() {
   let letters = '0123456789ABCDEF'.split('');
   let color = '#';
@@ -7,3 +11,17 @@ function getRandomColor() {
 
   return color;
 }
+
+let creationTme, reactionTime, clickTime;
+
+const makeBox = () => {
+  let time = Math.random() * 3000;
+
+  setTimeout(() => {
+    box.style.border = '1px solid gray';
+    box.style.backgroundColor = getRandomColor();
+    box.style.display = 'block';
+  }, time);
+};
+
+btn.addEventListener('click', makeBox);
